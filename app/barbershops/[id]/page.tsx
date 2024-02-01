10c47efa03620acafef5252da45d9da54699a68d
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-01-31 23:55:30 -0300

    feat: add ServiceItem component

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,48 @@
+import { db } from "@/app/_lib/prisma";
+import BarberInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
+
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string;
+  };
+}
+
+const BarbershopDetailsPage = async ({
+  params,
+}: BarbershopDetailsPageProps) => {
+  if (!params.id) {
+    // TODO: redirecionar para a Home page
+
+    return null;
+  }
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
+
+  if (!barbershop) {
+    // TODO: redirecionar para a Home page
+
+    return null;
+  }
+
+  return (
+    <div>
+      <BarberInfo barbershop={barbershop} />
+
+      <div className="flex flex-col gap-4 px-5 py-6">
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BarbershopDetailsPage;
