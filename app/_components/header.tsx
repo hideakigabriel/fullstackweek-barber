8eb5ffbcb91570f8531f930c8cc3ea69ef27d264
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-02-01 20:24:43 -0300

    feat: add AuthProvider

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
 import { MenuIcon } from "lucide-react";
+import { signIn, useSession } from "next-auth/react";
 
 const Header = () => {
   return (
@@ -10,8 +13,9 @@ const Header = () => {
         <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
 
         <Button variant="outline" size="icon" className="h-8 w-8">
-          <MenuIcon size={16}/>
+          <MenuIcon size={16} />
         </Button>
+        
       </CardContent>
     </Card>
   );
