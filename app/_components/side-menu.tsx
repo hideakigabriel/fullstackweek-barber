3c06b43983dedae48efa0175e84a76c3e4c8774d
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-02-01 23:05:29 -0300

    feat: add SideMenu component

---
+++ app/_components/side-menu.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Avatar } from "@radix-ui/react-avatar";
+import { SheetHeader, SheetTitle } from "./ui/sheet";
+import { AvatarImage } from "./ui/avatar";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "./ui/button";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon, UserIcon } from "lucide-react";
+import Link from "next/link";
+
+const SideMenu = () => {
+
+  const { data } = useSession();
+
+  const handleLogInClick = () => {
+    signIn("google");
+  };
+
+  const handleLogOutClick = () => {
+    signOut();
+  };
+
+  return (
+    <>
+      <SheetHeader className="text-left border-solid border-b border-secondary p-5">
+        <SheetTitle>Menu</SheetTitle>
+      </SheetHeader>
+
+      {data?.user ? (
+        <div className="flex items-center px-5 py-6 justify-between">
+          <div className="flex items-center gap-3">
+            <Avatar>
+              <AvatarImage src={data.user?.image ?? ""} />
+            </Avatar>
+
+            <h2 className="font-bold ">{data.user.name}</h2>
+          </div>
+
+          <Button onClick={handleLogOutClick} variant="logOut" size="icon">
+            <LogOutIcon />
+          </Button>
+        </div>
+      ) : (
+        <div className="flex flex-col px-5 py-6 gap-3">
+          <div className="flex items-center gap-2">
+            <UserIcon size={32} />
+            <h2 className="font-bold">Olá, faça seu login!</h2>
+          </div>
+
+          <Button
+            onClick={handleLogInClick}
+            variant="secondary"
+            className="w-full flex justify-start"
+          >
+            <LogInIcon className="mr-2" size={18} />
+            Fazer Login
+          </Button>
+        </div>
+      )}
+
+      <div className="flex flex-col gap-3 px-5">
+        <Button variant="outline" className="justify-start" asChild>
+          <Link href="/">
+            <HomeIcon className="mr-2" size={18} />
+            Início
+          </Link>
+        </Button>
+
+        {data?.user && (
+          <Button variant="outline" className="justify-start" asChild>
+            <Link href="/bookings">
+              <CalendarIcon className="mr-2" size={18} />
+              Agendamentos
+            </Link>
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default SideMenu;
--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -5,6 +5,8 @@ import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import { Barbershop } from "@prisma/client";
 import { useRouter } from "next/navigation";
+import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
+import SideMenu from "@/app/_components/side-menu";
 
 interface BarbershopInfoProps {
   barbershop: Barbershop;
@@ -29,13 +31,18 @@ const BarberInfo = ({ barbershop }: BarbershopInfoProps) => {
           <ChevronLeftIcon />
         </Button>
 
-        <Button
-          size="icon"
-          variant="outline"
-          className="z-50 absolute top-4 right-4"
-        >
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="z-50 absolute top-4 right-4">
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
+        
         <Image
           src={barbershop.imageUrl}
           fill
